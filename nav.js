8603c362dcd05cea36755b737a8e2f76c337bb98
ajhridoy <ajhridoy461@gmail.com>
2024-03-18 16:41:53 +0600

    updated responsive

--- nav.js
+++ nav.js
@@ -7,6 +7,9 @@ function scrollFunction() {
     document.getElementById("navbar2").style.width = "100%";
     document.getElementById("navbar2").style.top = "0";
     document.getElementById("navbar2").style.background = "#fff";
+    document.getElementById("dropdown-menu1").style.top = "57px"
+    document.getElementById("dropdown-menu2").style.top = "57px"
+    document.getElementById("dropdown-menu3").style.top = "57px"
     // document.getElementById("navbar2").style.transition = ".4s";
     // document.getElementById("navbar2").style.boxShadow = "0 0 30px rgba(0,0,0,.075)";
     document.getElementById("navbar2").style.borderBottom = "none";
@@ -14,21 +17,30 @@ function scrollFunction() {
   } else {
     document.getElementById("navbar2").style.position = "initial";
     document.getElementById("navbar2").style.top = "0";
+    document.getElementById("dropdown-menu1").style.top = "140px"
+    document.getElementById("dropdown-menu2").style.top = "140px"
+    document.getElementById("dropdown-menu3").style.top = "140px"
     document.getElementById("navbar2").style.padding = "38px 0";
     // document.getElementById("navbar2").style.background = "transparent";
     document.getElementById("navbar2").style.boxShadow = "none";
     document.getElementById("navbar2").style.borderBottom = "";
-    document.getElementById("navbar2").style.zIndex = "99";
+    document.getElementById("navbar2").style.zIndex = "999999";
   }
   if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
     
     // document.getElementById("navbar2").style.transition = ".4s";
     document.getElementById("navbar2").style.zIndex = "99";
+    document.getElementById("dropdown-menu1").style.top = "57px"
+    document.getElementById("dropdown-menu2").style.top = "57px"
+    document.getElementById("dropdown-menu3").style.top = "57px"
     document.getElementById("navbar2").style.background = "#ffff";
   }
   else {
    
     document.getElementById("navbar2").style.zIndex = "99";
+    document.getElementById("dropdown-menu1").style.top = "140px"
+    document.getElementById("dropdown-menu2").style.top = "140px"
+    document.getElementById("dropdown-menu3").style.top = "140px"
     
   }
 }
